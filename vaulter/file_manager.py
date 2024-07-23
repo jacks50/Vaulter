fa4c5222ba5c088f747b7623ea8afdc4b94e8b4e
Jacks <dani.moreira30@gmail.com>
2024-07-23 19:43:56 +0200

    Safety commit since PC is crashing a lot

--- vaulter/file_manager.py
+++ vaulter/file_manager.py
@@ -1,6 +1,8 @@
 import abc
+import os
 
 from flask import current_app
+from pathlib import Path
 
 
 def get_file_manager():
@@ -31,7 +33,7 @@ class FileManagerInterface(metaclass=abc.ABCMeta):
         return cls.instance
 
     @abc.abstractmethod
-    def file_create(self):
+    def file_create(self, file_name, file_content, file_path):
         """
         Creates a file in the designed storage used
         :return:
@@ -39,7 +41,7 @@ class FileManagerInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def file_get(self):
+    def file_get(self, file_name, file_path):
         """
         Retrieves an existing file in the designed storage used
         :return:
@@ -47,7 +49,7 @@ class FileManagerInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def file_update(self):
+    def file_update(self, file_name, file_content, file_path):
         """
         Updates an existing file in the designed storage used
         :return:
@@ -55,7 +57,7 @@ class FileManagerInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def file_delete(self):
+    def file_delete(self, file_name, file_content):
         """
         Deletes an existing file in the designed storage used
         :return:
@@ -63,7 +65,7 @@ class FileManagerInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def file_list(self):
+    def file_list(self, file_path):
         """
         List files in the designed storage used
         :return:
@@ -72,52 +74,85 @@ class FileManagerInterface(metaclass=abc.ABCMeta):
 
 
 class InternalFileManager(FileManagerInterface):
-    def file_create(self):
-        pass
 
-    def file_get(self):
-        pass
+    def _check_dir_file_exists(self, file_name, file_path):
+        file_dir = Path(file_path)
 
-    def file_update(self):
-        pass
+        if not (file_dir.exists() or file_dir.is_dir()):
+            raise FileNotFoundError
 
-    def file_delete(self):
-        pass
+        file_complete_path = file_dir / file_name
 
-    def file_list(self):
+        if file_complete_path.exists() or not file_complete_path.is_file():
+            raise FileExistsError
+
+        return file_complete_path
+
+    def file_create(self, file_name, file_content, file_path):
+        file_complete_path = self._check_dir_file_exists(file_name, file_path)
+
+        file_complete_path.touch()
+
+        return file_complete_path
+
+    def file_get(self, file_name, file_path):
+        file_complete_path = self._check_dir_file_exists(file_name, file_path)
+
+        return file_complete_path
+
+    def file_update(self, file_name, file_content, file_path):
+        file_complete_path = self._check_dir_file_exists(file_name, file_path)
+
+        with file_complete_path.open(mode='w') as f:
+            f.write(file_content)
+
+        return file_complete_path
+
+    def file_delete(self, file_name, file_path):
+        file_complete_path = self._check_dir_file_exists(file_name, file_path)
+
+        file_complete_path.unlink()
+
+        return True
+
+    def file_list(self, file_path):
+
+        for file in Path(file_path).iterdir():
+
         pass
 
 
 class PostgresFileManager(FileManagerInterface):
-    def file_create(self):
+
+    def file_create(self, file_name, file_content, file_path):
         pass
 
-    def file_get(self):
+    def file_get(self, file_name, file_path):
         pass
 
-    def file_update(self):
+    def file_update(self, file_name, file_content, file_path):
         pass
 
-    def file_delete(self):
+    def file_delete(self, file_name, file_content):
         pass
 
-    def file_list(self):
+    def file_list(self, file_path):
         pass
 
 
 class S3FileManager(FileManagerInterface):
-    def file_create(self):
+
+    def file_create(self, file_name, file_content, file_path):
         pass
 
-    def file_get(self):
+    def file_get(self, file_name, file_path):
         pass
 
-    def file_update(self):
+    def file_update(self, file_name, file_content, file_path):
         pass
 
-    def file_delete(self):
+    def file_delete(self, file_name, file_content):
         pass
 
-    def file_list(self):
+    def file_list(self, file_path):
         pass
-
