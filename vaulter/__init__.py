f8b662863d5cf2a54874c4217723f237cf054b73
Daniel Moreira <dani.moreira30@gmail.com>
2024-08-24 14:42:16 +0200

    Add CORS to enable cross origin requests

--- vaulter/__init__.py
+++ vaulter/__init__.py
@@ -1,9 +1,12 @@
 import os
 
 from flask import Flask, render_template
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_object(f'config.{os.getenv("VAULTER_APP", "Development")}Config')
     
     from .vaulter import vaulture_bp
